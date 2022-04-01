42c0d0b5c9dc909c8929142bed3fdcff668a1b1d
Kevin Genus <kevin.genus@gmail.com>
2022-04-01 06:44:54 -0400

    Adds support for css modules and svg

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -7,16 +8,43 @@ module.exports = {
     extensions: [".ts", ".tsx",  ".js"]
   },
   module: {
-    rules: [{
-      test: /\.tsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { modules: true } },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: '@svgr/webpack',
+        options: {
+          svgoConfig: {
+            plugins: [{
+              name: 'removeViewBox',
+              active: false,
+            }]
+          }
+        }
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename: 'index.html'
-    })
-  ]
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
 }
